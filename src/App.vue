9c4aeaaa77578d6122e096ba61960f8f15755da8
Thisura Rasanjana <23707@srilankan.com>
2022-04-19 12:28:15 +0530

    navbar start

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <MainMenu />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import MainMenu from './components/Header/MainMenu.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+    MainMenu
   }
 }
 </script>
@@ -19,8 +22,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
 }
 </style>
--- src/components/Header/MainMenu.vue
+++ src/components/Header/MainMenu.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h1>NavBar</h1>
+        <h1 class="text-center">NavBar</h1>
     </div>
 </template>
 
